36346b1a613238db7b0c2ededa0c5687806406d8
jeongsupark <jsuuu96@naver.com>
2025-02-13 10:14:11 +0900

    2025. 02. 12
    점프 애니메이션, Blendspace 적용

--- Source/TPSExample/Characters/CTPSAnimInstance.h
+++ Source/TPSExample/Characters/CTPSAnimInstance.h
@@ -17,4 +17,19 @@ public:
 	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "TPS")
 	float Speed = 0;
 
+	// 플레이어 좌우 이동속도
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "TPS")
+	float Direction = 0;
+
+	// 공중에 있는 상태인지 확인할 Bool변수
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "TPS")
+	bool IsInAir = false;
+
+	// 재생할 공격 애니메이션 몽타주
+	UPROPERTY(EditDefaultsOnly, Category = "TPS")
+	class UAnimMontage* AttackAnimMontage;
+
+public:
+	void PlayAttackAnim();
+
 };
